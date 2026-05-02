36bed34e56260ca899771b7ec77a717d30290493
Non <43197300+nonperforming@users.noreply.github.com>
2026-05-02 20:35:05 +1200

    scnBaseExtensions.GoToLevelWithCustomLevelData

---
+++ Extensions/scnBaseExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace PulseLib.Extensions;
+
+public static class scnBaseExtensions
+{
+  public static void GoToLevelWithCustomLevelData(CustomLevelData levelData, bool twoPlayer) =>
+    scnBase.GoToLevelWithExternalPath(
+      twoPlayer
+      && !levelData.isLegacyLevel
+      && levelData.settings.canBePlayedOn == LevelPlayMode.BothModes
+      && !string.IsNullOrEmpty(levelData.settings.separate2PLevelFilename)
+        ? Path.Combine(levelData.path, levelData.settings.separate2PLevelFilename)
+        : Path.Combine(levelData.path, "main.rdlevel")
+    );
+}
